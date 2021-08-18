067c4891134c6c8812c7eb7710daae1ece74a260
Cooper Lorsung <clorsung@andrew.cmu.edu>
2021-08-18 13:50:30 -0400

    Updated test to import augment functions and fail all tests for now

--- test/test_augment.py
+++ test/test_augment.py
@@ -1,2 +1,15 @@
-def test():
-    assert True
+import sys
+sys.path.append(sys.path[0][:-4])
+
+from auglichem.molecule.transforms import smiles2graph, RandomAtomMask, RandomBondDelete
+
+
+def test_atom_mask():
+    assert False
+
+def test_bond_delete():
+    assert False
+
+def test_smiles2graph():
+    assert False
+
