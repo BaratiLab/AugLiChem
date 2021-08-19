8f5971fbe245c3a9e0ac97e0fc8cf914586b7089
CoopLo <clorsung@andrew.cmu.edu>
2021-08-19 16:51:12 -0400

    Updated directory structure to approximately follow AugLy structure. Have _transformation.py that holds atom masking and bond deletion. _composition.py is unused currently, but can offer a base class to build combinations of atom and bond augmentation.  holds scripts for loading data sets and storing them as an object. Currently only supports the BACE dataset. Need to expand to the rest of the moleculenet data sets. This will involve making sure datasets with multiple targets are loaded properly, and that datasets that are downloaded with a  extension are extracted and loaded properly.

---
+++ auglichem/molecule/data/_molecule_dataset.py
@@ -0,0 +1,275 @@
+import os
+import csv
+import math
+import random
+import numpy as np
+from copy import deepcopy
+
+import torch
+import torch.nn.functional as F
+from torch_geometric.data import Data, Dataset, DataLoader
+
+import rdkit
+from rdkit import Chem
+from rdkit.Chem.rdchem import BondType as BT
+
+from auglichem.utils import (
+        ATOM_LIST,
+        CHIRALITY_LIST,
+        BOND_LIST,
+        BONDDIR_LIST,
+        random_split,
+        scaffold_split
+)
+from auglichem.molecule import RandomAtomMask, RandomBondDelete
+#from auglichem.molecule.data import read_smiles
+from ._load_sets import read_smiles
+
+
+#TODO docstrings for MoleculeData
+
+class Data(Dataset):
+    def __init__(self, smiles_data, labels, task, test_mode=True, aug_time=1,
+                 node_mask_ratio=[0, 0.25], edge_mask_ratio=[0, 0.25]):
+        '''
+            Initialize Molecular Data set object. This object tracks data, labels,
+            task, test, and augmentation
+
+            Input:
+            -----------------------------------
+            smiles_data (np.ndarray of str): Data set in smiles format
+            labels (np.ndarray of float): Data labels, maybe optional if unsupervised?
+            task (str): 'regression' or 'classification' indicating the learning task
+            test_mode (boolean, default=True): Does no augmentations if true
+            aug_time (int, optional, default=1): Controls augmentations (not quite sure how yet)
+            node_mask_ratio (list, float): If list, sample mask ratio uniformly over [a, b]
+                                           where a < b, if float, set ratio to input.
+            edge_mask_ratio (list, float): If list, sample mask ratio uniformly over [a, b]
+                                           where a < b, if float, set ratio to input.
+
+
+            Output:
+            -----------------------------------
+            None
+        '''
+        super(Dataset, self).__init__()
+
+        # Store class attributes
+        self.smiles_data = smiles_data
+        self.labels = labels
+        self.task = task
+        self.test_mode = test_mode
+        self.aug_time = aug_time
+        if self.test_mode:
+            self.aug_time = 1
+        assert type(aug_time) == int
+        assert aug_time >= 1
+
+        # For reproducibility
+        self.reproduce_seeds = list(range(self.__len__()))
+        np.random.shuffle(self.reproduce_seeds)
+
+        # Store mask ratios
+        self.node_mask_ratio = node_mask_ratio
+        self.edge_mask_ratio = edge_mask_ratio
+
+
+    def _get_data_x(self, mol):
+        '''
+            Get the transformed data features.
+
+            Inputs:
+            -----------------------------------
+            mol ( object): Current molecule
+
+            Outputs:
+            -----------------------------------
+            x (torch.Tensor of longs):
+        '''
+
+        # Set up data arrays
+        type_idx, chirality_idx, atomic_number = [], [], []
+
+        # Gather atom data
+        for atom in mol.GetAtoms():
+            type_idx.append(ATOM_LIST.index(atom.GetAtomicNum()))
+            chirality_idx.append(CHIRALITY_LIST.index(atom.GetChiralTag()))
+            atomic_number.append(atom.GetAtomicNum())
+
+        # Concatenate atom type with chirality index
+        x1 = torch.tensor(type_idx, dtype=torch.long).view(-1,1)
+        x2 = torch.tensor(chirality_idx, dtype=torch.long).view(-1,1)
+        x = torch.cat([x1, x2], dim=-1)
+
+        return x
+
+
+    def _get_data_y(self, index):
+        '''
+            Get the transformed data label.
+
+            Inputs:
+            -----------------------------------
+            index (int): Index for current molecule
+
+            Outputs:
+            -----------------------------------
+            y (torch.Tensor, long if classification, float if regression): Data label
+        '''
+
+        if self.task == 'classification':
+            y = torch.tensor(self.labels[index], dtype=torch.long).view(1,-1)
+        elif self.task == 'regression':
+            y = torch.tensor(self.labels[index], dtype=torch.float).view(1,-1)
+
+        return y
+
+
+    def _get_edge_index_and_attr(self, mol):
+        '''
+            Create the edge index and attributes
+
+            Inputs:
+            -----------------------------------
+            mol ():
+
+            Outputs:
+            -----------------------------------
+            edge_index ():
+            edge_attr ():
+        '''
+
+        # Set up data collection lists
+        row, col, edge_feat = [], [], []
+        for bond in mol.GetBonds():
+
+            # Get the beginning and end atom indices
+            start, end = bond.GetBeginAtomIdx(), bond.GetEndAtomIdx()
+
+            # Store bond atoms
+            row += [start, end]
+            col += [end, start]
+
+            # Store edge featuers
+            edge_feat.append([
+                BOND_LIST.index(bond.GetBondType()),
+                BONDDIR_LIST.index(bond.GetBondDir())
+            ])
+            edge_feat.append([
+                BOND_LIST.index(bond.GetBondType()),
+                BONDDIR_LIST.index(bond.GetBondDir())
+            ])
+
+        # Create edge index and attributes
+        edge_index = torch.tensor([row, col], dtype=torch.long)
+        edge_attr = torch.tensor(np.array(edge_feat), dtype=torch.long)
+
+        return edge_index, edge_attr
+
+
+    def __getitem__(self, index):
+        '''
+            Selects an element of self.smiles_data according to the index.
+            Edge and node masking are done here for each individual molecule
+
+            Input:
+            -----------------------------------
+            index (int): Index of molecule we would like to augment
+
+            Output:
+            -----------------------------------
+            masked_data (Data object): data that has been augmented with node and edge masking
+
+        '''
+
+        # If augmentation is done, actual dataset is smaller than given indices
+        if self.test_mode:
+            true_index = index
+        else:
+            true_index = index // self.aug_time
+
+
+        # Create initial data set
+        mol = Chem.MolFromSmiles(self.smiles_data[true_index]) # Need an index somehow
+        mol = Chem.AddHs(mol)
+
+        # Get data x and y
+        x = self._get_data_x(mol)
+        y = self._get_data_y(true_index)
+
+        # Get edge index and attributes
+        edge_index, edge_attr = self._get_edge_index_and_attr(mol)
+
+        # Store number of atoms and bonds
+        num_atoms = mol.GetNumAtoms()
+        num_bonds = mol.GetNumBonds()
+
+        # Mask according to initialized rules
+        x_mask = self.mask_nodes(x, num_atoms)
+        edge_index_mask, edge_attr_mask = self.mask_edges(edge_index, edge_attr, num_bonds)
+
+        return Data(x=x_mask, y=y, edge_index=edge_index_mask, edge_attr=edge_attr_mask)
+
+
+    def __len__(self):
+        return len(self.smiles_data) * self.aug_time
+
+
+class MoleculeData(Data):
+    def __init__(self, dataset, split="scaffold", batch_size=64, num_workers=0,
+                 valid_size=0.1, test_size=0.1, aug_time=1, data_path=None):
+        '''
+            Input:
+            ---
+            dataset (str): One of the datasets available from MoleculeNet
+                           (http://moleculenet.ai/datasets-1)
+            split (str, default=scaffold): random or scaffold. The splitting strategy used for
+                         train/test/validation set creation.
+            batch_size (int, default=64): Batch size used in training
+            num_workers (int, default=0): Number of workers used in loading data
+            valid_size (float in [0,1], default=0.1): 
+
+
+            Output:
+            ---
+            None
+        '''
+        super(object, self).__init__()
+        self.dataset = dataset
+        self.split = split
+        self.data_path = data_path
+        self.batch_size = batch_size
+        self.num_workers = num_workers
+        self.valid_size = valid_size
+        self.test_size = test_size
+        self.aug_time = aug_time
+        self.smiles_data, self.labels = read_smiles(dataset, data_path)
+        self.smiles_data = np.asarray(self.smiles_data)
+        self.labels = np.asarray(self.labels)
+
+    def get_data_loaders(self):
+        train_idx, valid_idx, test_idx = scaffold_split(self.smiles_data, self.valid_size,
+                                                        self.test_size)
+
+        # define dataset
+        train_set = MolDataset(self.smiles_data[train_idx], self.labels[train_idx],
+                               test_mode=False, aug_time=self.aug_time, task=self.task)
+        valid_set = MolDataset(self.smiles_data[valid_idx], self.labels[valid_idx],
+                               test_mode=True, task=self.task)
+        test_set = MolDataset(self.smiles_data[test_idx], self.labels[test_idx],
+                              test_mode=True, task=self.task)
+
+        train_loader = DataLoader(
+            train_set, batch_size=self.batch_size,
+            num_workers=self.num_workers, drop_last=True, shuffle=True
+        )
+        valid_loader = DataLoader(
+            valid_set, batch_size=self.batch_size,
+            num_workers=self.num_workers, drop_last=False
+        )
+        test_loader = DataLoader(
+            test_set, batch_size=self.batch_size,
+            num_workers=self.num_workers, drop_last=False
+        )
+
+        return train_loader, valid_loader, test_loader
