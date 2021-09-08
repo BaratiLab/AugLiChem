6e6bf89596ff2a9c5896a73d348e34635f75ab35
CoopLo <clorsung@andrew.cmu.edu>
2021-09-08 18:16:51 -0400

    Updated testing scripts

--- test/test_utils.py
+++ test/test_utils.py
@@ -21,22 +21,47 @@ RDLogger.DisableLog('rdApp.*')
 
 def test_scaffold_split():
     # Test molecule
-    dataset = MoleculeDatasetWrapper('BACE', split='scaffold')
+    dataset = MoleculeDatasetWrapper('BACE', split='scaffold', seed=0)
+
+    # First split
     train, val, test = dataset.get_data_loaders()
+    load_idxs = np.concatenate([dataset.train_idx, dataset.valid_idx, dataset.test_idx])
+
+    # All splits are the same
+    train, val, test = dataset.get_data_loaders()
+    load_idxs1 = np.concatenate([dataset.train_idx, dataset.valid_idx, dataset.test_idx])
+
     all_idxs = list(range(len(dataset)))
-    load_idxs = np.sort(np.concatenate([dataset.train_idx, dataset.valid_idx, dataset.test_idx]))
-    assert all(all_idxs == load_idxs)
+    assert all(all_idxs == np.sort(load_idxs))
+    assert all(all_idxs == np.sort(load_idxs1))
+    assert all(load_idxs == load_idxs1)
 
     # Test crystal
     assert True
 
 def test_random_split():
     # Test molecule
-    dataset = MoleculeDatasetWrapper('BACE', split='random')
+    dataset = MoleculeDatasetWrapper('BACE', split='random', seed=0)
+
+    # First split
     train, val, test = dataset.get_data_loaders()
+    load_idxs = np.concatenate([dataset.train_idx, dataset.valid_idx, dataset.test_idx])
+
+    # Second split should be the same
+    train, val, test = dataset.get_data_loaders()
+    load_idxs1 = np.concatenate([dataset.train_idx, dataset.valid_idx, dataset.test_idx])
+
     all_idxs = list(range(len(dataset)))
-    load_idxs = np.sort(np.concatenate([dataset.train_idx, dataset.valid_idx, dataset.test_idx]))
-    assert all(all_idxs == load_idxs)
+    assert all(all_idxs == np.sort(load_idxs))
+    assert all(all_idxs == np.sort(load_idxs1))
+    assert all(load_idxs == load_idxs1)
+
+    # Third split should be different
+    dataset = MoleculeDatasetWrapper('BACE', split='random', seed=1)
+    train, val, test = dataset.get_data_loaders()
+    load_idxs2 = np.concatenate([dataset.train_idx, dataset.valid_idx, dataset.test_idx])
+    assert any(load_idxs != load_idxs2)
+
 
     # Test crystal
     assert True
@@ -54,6 +79,6 @@ def test_constants():
     assert len(BONDDIR_LIST) == 4
     assert NUM_BOND_DIRECTION == 4
 
-#if __name__ == '__main__':
-#    test_scaffold_split()
-#    test_random_split()
+if __name__ == '__main__':
+    #test_scaffold_split()
+    test_random_split()
