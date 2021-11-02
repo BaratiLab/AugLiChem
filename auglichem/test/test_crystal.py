68c5e66128a597a29f0d232723da4d37023ebdb3
CoopLo <clorsung@andrew.cmu.edu>
2021-11-02 17:28:38 -0400

    Finished testing transforms

--- auglichem/test/test_crystal.py
+++ auglichem/test/test_crystal.py
@@ -36,9 +36,8 @@ def test_data_download():
     dir_names = dict(zip(datasets, ["lanths","band","abx3_cifs","FE","fermi"]))
     for d in datasets:
         dataset = CrystalDatasetWrapper(d, batch_size=1)
-        os.path.isdir("./data_download/{}".format(dir_names[d]))
+        assert os.path.isdir("./data_download/{}".format(dir_names[d]))
     shutil.rmtree("./data_download")
-    assert True
     
 
 def _check_id_prop_augment(path, transform):
@@ -108,9 +107,8 @@ def _check_completeness(path, fold):
 
 def test_k_fold():
     #assert True
-    #TODO: Automatic data downloading - this works locally
     dataset = CrystalDatasetWrapper("lanthanides", kfolds=5,
-                                    data_path="../../examples/data_download")
+                                    data_path="./data_download")
     transform = [SupercellTransformation()]
 
     # Check no repeated indices in train and valid
@@ -156,7 +154,7 @@ def test_k_fold():
     shutil.rmtree(dataset.data_path)
 
     dataset = CrystalDatasetWrapper("lanthanides", kfolds=2,
-                                    data_path="../../examples/data_download")
+                                    data_path="./data_download")
     transform = [SupercellTransformation(), PerturbStructureTransformation()]
 
     # Check no repeated indices in train and valid
@@ -178,129 +176,182 @@ def test_k_fold():
 
 
 def test_rotation():
-    transform = RotationTransformation([0,0,1], 90)
 
     coords = [[0, 0, 0], [0.75,0.5,0.75]]
     lattice = Lattice.from_parameters(a=3.84, b=3.84, c=3.84, alpha=120,
                                   beta=90, gamma=60)
     struct = Structure(lattice, ["Si", "Si"], coords)
     
-    print(struct)
-    struct = transform.apply_transformation(struct)
-    print(struct)
-    pass
+    transform = RotationTransformation([1,1,1], 360)
+    struct1 = transform.apply_transformation(struct)
+    assert struct == struct1
+
+    transform = RotationTransformation([1,0,0], 180)
+    struct2 = transform.apply_transformation(struct)
+    assert np.allclose(struct.lattice.matrix[1], -struct2.lattice.matrix[1], atol=1e-8)
+    assert np.allclose(struct.lattice.matrix[2], -struct2.lattice.matrix[2], atol=1e-8)
 
 
 def test_perturb_structure():
-    transform = PerturbStructureTransformation()
 
     coords = [[0, 0, 0], [0.75,0.5,0.75]]
     lattice = Lattice.from_parameters(a=3.84, b=3.84, c=3.84, alpha=120,
                                   beta=90, gamma=60)
     struct = Structure(lattice, ["Si", "Si"], coords)
     
-    print(struct)
-    struct = transform.apply_transformation(struct)
-    print(struct)
-    pass
+    transform = PerturbStructureTransformation(distance=1, min_distance=0.1)
+    struct1 = transform.apply_transformation(struct.copy(), seed=1)
+
+    struct = Structure(lattice, ["Si", "Si"], coords)
+    struct2 = transform.apply_transformation(struct.copy(), seed=1)
+
+    struct = Structure(lattice, ["Si", "Si"], coords)
+    struct3 = transform.apply_transformation(struct.copy(), seed=2)
+
+    assert struct1 == struct2
+    assert not(struct1 == struct3)
+    assert not(struct2 == struct3)
+
+    # We have to trust Pymatgen's implementation is correct here since so much is under the
+    # hood for us. No distance to perturb results in an identical structure.
+    transform = PerturbStructureTransformation(distance=0)
+    struct = Structure(lattice, ["Si", "Si"], coords)
+    struct1 = transform.apply_transformation(struct.copy(), seed=2)
+
+    assert struct == struct1
 
 
 def test_remove_sites():
-    transform = RemoveSitesTransformation([0])
 
     coords = [[0, 0, 0], [0.75,0.5,0.75]]
     lattice = Lattice.from_parameters(a=3.84, b=3.84, c=3.84, alpha=120,
                                   beta=90, gamma=60)
     struct = Structure(lattice, ["Si", "Si"], coords)
     
-    print(struct)
-    struct = transform.apply_transformation(struct)
-    print(struct)
-    pass
+    # Remove first site
+    transform = RemoveSitesTransformation([0])
+    struct1 = transform.apply_transformation(struct)
+    assert len(struct1.sites) == 1
+    assert struct.sites[1] == struct1.sites[0]
+
+    # Remove second site
+    struct = Structure(lattice, ["Si", "Si"], coords)
+    transform = RemoveSitesTransformation([1])
+    struct2 = transform.apply_transformation(struct)
+    assert len(struct2.sites) == 1
+    assert struct.sites[0] == struct2.sites[0]
 
 
 def test_supercell():
-    transform = SupercellTransformation()
+    transform = SupercellTransformation([[2,0,0],[0,2,0],[0,0,2]])
 
     coords = [[0, 0, 0], [0.75,0.5,0.75]]
     lattice = Lattice.from_parameters(a=3.84, b=3.84, c=3.84, alpha=120,
                                   beta=90, gamma=60)
     struct = Structure(lattice, ["Si", "Si"], coords)
     
-    print(struct)
-    struct = transform.apply_transformation(struct)
-    print(struct)
-    pass
+    struct1 = transform.apply_transformation(struct)
+    assert len(struct1.sites) == 8*len(struct.sites)
+    assert np.allclose(struct1.lattice.abc, np.multiply(struct.lattice.abc,2), atol=1e-8)
+
+    #TODO is more in depth testing here needed considering we use pymatgen's built-in function?
 
 
 def test_translate():
-    transform = TranslateSitesTransformation([0], [1,0,0], True)
+    transform = TranslateSitesTransformation(indices_to_move=[0], translation_vector=[1,1,1],
+                                             vector_in_frac_coords=True)
 
     coords = [[0, 0, 0], [0.75,0.5,0.75]]
     lattice = Lattice.from_parameters(a=3.84, b=3.84, c=3.84, alpha=120,
                                   beta=90, gamma=60)
     struct = Structure(lattice, ["Si", "Si"], coords)
     
-    print(struct)
-    struct = transform.apply_transformation(struct)
-    print(struct)
-    pass
+    # No translation
+    struct1 = transform.apply_transformation(struct)
+    assert struct1 == struct
 
-
-def test_cubic_supercell():
-    transform = CubicSupercellTransformation()
-
-    coords = [[0, 0, 0], [0.75,0.5,0.75]]
-    lattice = Lattice.from_parameters(a=3.84, b=3.84, c=3.84, alpha=120,
-                                  beta=90, gamma=60)
+    # No fractional coordinates
+    transform = TranslateSitesTransformation(indices_to_move=[0], translation_vector=[0.2,0.2,0.2],
+                                             vector_in_frac_coords=False)
     struct = Structure(lattice, ["Si", "Si"], coords)
-    
-    print(struct)
-    struct = transform.apply_transformation(struct)
-    print(struct)
-    pass
+    struct2 = transform.apply_transformation(struct)
+    assert np.allclose(struct2.sites[0].coords, [0.2, 0.2, 0.2], atol=1e-8)
 
-
-def test_primitive():
-    transform = PrimitiveCellTransformation()
-
-    coords = [[0, 0, 0], [0.75,0.5,0.75]]
-    lattice = Lattice.from_parameters(a=3.84, b=3.84, c=3.84, alpha=120,
-                                  beta=90, gamma=60)
+    # Fractional coordinates
+    transform = TranslateSitesTransformation(indices_to_move=[0],
+                                             translation_vector=[0.2,0,0],
+                                             vector_in_frac_coords=True)
     struct = Structure(lattice, ["Si", "Si"], coords)
-    
-    print(struct)
-    struct = transform.apply_transformation(struct)
-    print(struct)
-    pass
+    struct3 = transform.apply_transformation(struct)
+    coord1 = struct.lattice.abc[0] * 0.2
+    assert np.allclose(struct3.sites[0].coords, [coord1,0,0], atol=1e-8)
+
+
+#def test_cubic_supercell():
+#    transform = CubicSupercellTransformation()
+#
+#    coords = [[0, 0, 0], [0.75,0.5,0.75]]
+#    lattice = Lattice.from_parameters(a=3.84, b=3.84, c=3.84, alpha=120,
+#                                  beta=90, gamma=60)
+#    struct = Structure(lattice, ["Si", "Si"], coords)
+#    
+#    print(struct)
+#    struct1 = transform.apply_transformation(struct)
+#    print(struct)
+#    pass
+#
+#
+#def test_primitive():
+#    transform = PrimitiveCellTransformation()
+#
+#    coords = [[0, 0, 0], [0.75,0.5,0.75]]
+#    lattice = Lattice.from_parameters(a=3.84, b=3.84, c=3.84, alpha=120,
+#                                  beta=90, gamma=60)
+#    struct = Structure(lattice, ["Si", "Si"], coords)
+#    
+#    print(struct)
+#    struct = transform.apply_transformation(struct)
+#    print(struct)
+#    pass
 
 
 def test_swap_axes():
-    #TODO: This does not work yet.
-    assert True
+    #TODO: Test consistent augment
     transform = SwapAxesTransformation()
 
     coords = [[0, 0, 0], [0.75,0.5,0.75]]
     lattice = Lattice.from_parameters(a=3.84, b=3.84, c=3.84, alpha=120,
                                   beta=90, gamma=60)
-    struct = Structure(lattice, ["Si", "Si"], coords)
     
-    print(struct)
-    struct = transform.apply_transformation(struct)
-    print(struct)
-    pass
+    struct = Structure(lattice, ["Si", "Si"], coords)
+    struct1 = transform.apply_transformation(struct, seed=1)
+    struct2 = transform.apply_transformation(struct, seed=2)
+    struct3 = transform.apply_transformation(struct, seed=1)
+
+    assert struct1 != struct2
+    assert struct1 == struct3
+
+    struct4 = transform.apply_transformation(struct, _test_choice=[0,2])
+    assert np.allclose(struct.sites[1].coords, struct1.sites[1].coords[::-1], atol=1e-8)
+
+    struct5 = transform.apply_transformation(struct, _test_choice=[0,1])
+    coords = struct.sites[1].coords
+    coords5 = struct5.sites[1].coords
+
+    assert np.isclose(coords[0], coords5[1], atol=1e-8)
+    assert np.isclose(coords[1], coords5[0], atol=1e-8)
+    assert np.isclose(coords[2], coords5[2], atol=1e-8)
     
 
 #if __name__ == '__main__':
-    #test_crystal_data()
     #test_data_download()
     #test_composition()
     #test_k_fold()
     #test_rotation()
     #test_perturb_structure()
-    #test_remove_sites()
+    ##test_remove_sites()
     #test_supercell()
     #test_translate()
-    #test_cubic_supercell()
-    #test_primitive()
+    ##test_cubic_supercell()
+    ##test_primitive()
     #test_swap_axes()
