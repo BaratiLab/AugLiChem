1fb8537160f464f77863173209c3ee690089038e
CoopLo <clorsung@andrew.cmu.edu>
2022-01-19 23:17:42 -0500

    Updated version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as f:
 setuptools.setup(
   name = 'auglichem',
   packages=setuptools.find_packages(exclude=("docs","examples",)), 
-  version = '0.1.2',
+  version = '0.1.3',
   license = 'MIT',
   description = 'Data augmentation of molecules and crystals.',
   long_description = long_description,
